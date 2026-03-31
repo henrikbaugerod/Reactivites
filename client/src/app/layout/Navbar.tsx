1f9c7b1785bd593e6746d03bff889fb77ff77213
Henrik Baugerød <henrikbaugerod@hotmail.com>
2026-03-31 14:22:55 +0200

    End of section 10

--- client/src/app/layout/Navbar.tsx
+++ client/src/app/layout/Navbar.tsx
@@ -30,6 +30,9 @@ export const Navbar = () => {
 							<MenuItemLink to='/counter'>
 								Counter
 							</MenuItemLink>
+							<MenuItemLink to='/errors'>
+								Errors
+							</MenuItemLink>
 						</Box>
 						<MenuItem>
 							User menu
